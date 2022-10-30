a39ef7dccef2249d5c89dc58fc05a251131b5023
Lucas Bordoni <lucasbordoni1612@gmail.com>
2022-10-30 19:04:02 -0300

    Token Hability

--- app/Lib/Main/Main.php
+++ app/Lib/Main/Main.php
@@ -4,4 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 use App\Lib\Main\Factories\Apis\Auth\AuthApiFactory;
 
+/**
+ * Authenticate a user
+ */
 Route::post("/auth", [AuthApiFactory::class, 'makeAuthApi']);
+
+/**
+ * Check if user is auth
+ */
+Route::get("/check-auth", function (){
+    return response()->json(['auth' => true]);
+})->middleware(['auth:sanctum', 'abilities:send:sms']);
